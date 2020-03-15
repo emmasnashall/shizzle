43af89c784a82a86c51f080039a6936a34081899
emmasnashall <emmasnashall@hotmail.com>
2020-03-15 21:40:18 +0000

    blah blah

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # shizzle
+to get something onto git from computer 
+git status 
+git add . 
+git commit -m 'whatever message you want'
+git push
 
---
+++ basic_maths.py
@@ -0,0 +1,45 @@
+print(2*3)
+print(2+3)
+
+print(2/3)
+print(2//3)
+print(2%3)
+
+a=5
+b=7
+c=a+b
+
+print(c)
+print(a**2)
+
+y=float(2)
+x=float(3)
+u=float(4)
+v=float(7)
+z=y/x
+print(float(z))
+
+#wiki exercise 1
+print(2**2**2**2)
+
+
+#wiki exercise 2
+t=(x+y)**u
+
+print(t)
+
+print(float(t/7))
+
+#wiki exercise 3
+q=float(10)
+p=float(9)
+r=float(q/p)
+print(float(r))
+
+print((10**20)/9)+(2*((10**20)/9))
+
+
+
+
+
+
